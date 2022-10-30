53d9dd88d0f258557417f77ea9050d24a44d8214
Henry-c-k <henryckleineidam@gmail.com>
2022-10-30 14:21:43 +0100

    game of life first version

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
+import numpy as np
+import pygame as pg
+import random
+from collections import Counter
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def neighboringCells(position):
+    for x,y in [(-1,-1),(0,-1),(1,-1),
+                (-1,0),(1,0),
+                (-1,1),(0,1),(1,1)]:
+        yield position[0] + x, position[1] + y
+def nextGenerationField(field):
+    neighbors = Counter([position for cell in field for position in neighboringCells(cell)])
+    return {position for position, counter in neighbors.items() if counter==3 or (counter==2 and position in field)}
+
+def getFieldSize(field, width, height):
+    Xs, Ys = zip(*field)
+    minX, maxX, minY, maxY = min(Xs), max(Xs), min(Ys), max(Ys)
+    columns, rows= maxX-minX+1, maxY-minY+1
+    return width/columns, height/rows, -minX, -minY
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+field = {(random.randrange(200),random.randrange(200)) for I in range(2000)}
+
+pg.init()
+screenWidth = screenHeight = 1000
+sceen = pg.display.set_mode([screenWidth, screenHeight])
+
+while True:
+    sceen.fill((0,0,0))
+    field = nextGenerationField(field)
+    for x,y in field:
+        cellWidth, cellHeight, offsetX, offsetY = getFieldSize(field, screenWidth, screenHeight)
+        pg.draw.rect(sceen, (255,0,0),((x+offsetX)*cellWidth, (y+offsetY)*cellHeight, 5, 5))
+    pg.display.flip()
+pg.quit()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
